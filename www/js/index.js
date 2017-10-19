9eb10928f1a2c5da5354a10105b221223a07574f
rmcoombe <coor2_16@uni.worc.ac.uk>
2017-10-19 15:04:20 +0100

    ffgdg
    sfgsrdfger

--- www/js/index.js
+++ www/js/index.js
@@ -1,34 +1,39 @@
 
-var key = “phone”;
-var value = “PixelXL”;
-
-window.localStorage.setItem( key, value );
-
-var key = “ID”;
-var value = “Student”;
-
-window.localStorage.setItem( key, value );
-
-var key = “card”;
-var value = "Lloyds”;
-
-window.localStorage.setItem( key, value );
-
-var key = “pen”;
-var value = “blue”;
-
-window.localStorage.setItem( key, value );
-
-var key = “pad”;
-var value = “Ruled”;
-
-window.localStorage.setItem( key, value );
+window.localStorage.setItem("phone", "pixelXL" );
+window.localStorage.setItem( "ID", "Student" );
+window.localStorage.setItem( "card", "lloyds" );
+window.localStorage.setItem( "pen", "blue" );
+window.localStorage.setItem( "pad", "ruled" );
 
 
 function showItem1(){
-	var key = "phone";
-	var value = window.localStorage.getItem(key);
-	alert (value)
+	
+	var value = window.localStorage.getItem("phone");
+	alert (value);
+}
+	
+function showItem2(){
+	
+	var value = window.localStorage.getItem("ID");
+	alert (value);
+}
+	
+	function showItem3(){
+	
+	var value = window.localStorage.getItem("card");
+	alert (value);
+}
+	
+	function showItem4(){
+	
+	var value = window.localStorage.getItem("pen");
+	alert (value);
+}
+	
+	function showItem5(){
+	
+	var value = window.localStorage.getItem("pad");
+	alert (value);
 }
 	
 	
