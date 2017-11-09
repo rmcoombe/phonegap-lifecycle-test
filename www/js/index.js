beff28abdc39d4cb92df80bc7f3b399de86b23ef
rmcoombe <coor2_16@uni.worc.ac.uk>
2017-11-09 14:18:43 +0000

    dfbb
    dfbdb

--- www/js/index.js
+++ www/js/index.js
@@ -1,43 +1,15 @@
-var chocbar ={
-	"name": "Wispa",
-	"Calories" : "215",
-	"Fat" : "13.3g",
-	"Saturates" : "8.2g",
-	"Sugars" : "20.5g",
-	"Salt" : "0.09g"
+$(document).on("pagecreate","#pageone",function(){
+  $('#submitButton').on("click", function(){
+    submitText();
+  });            
+});            
+
+
+function submitText() {
+	var text = $('#textinput').val();
+	alert(text);
 }
 
-var	chocbarString = JSON.stringify(chocbar);
-
-window.localStorage.setItem("chocbar", chocbarString);
-
-var getchocbar = window.localStorage.getItem("chocbar");
-
-var detailJSON = JSON.parse(getchocbar);
-
-
-function calories(){
-	
-	alert ("The Calories in a " + chocbar.name + " are " + chocbar.Calories);
-
-}
-function fat(){
-	
-	alert ("The fat in a " + chocbar.name + " is " + chocbar.Fat);
-
-}
-function saturates(){
-	
-	alert ("The saturates in a " + chocbar.name + " are " + chocbar.Saturates);
-
-}
-function sugars(){
-	
-	alert ("The suagrs in a " + chocbar.name + " are " + chocbar.Sugars);
-
-}
-function salt(){
-	
-	alert ("The salt in a " + chocbar.name + " are " + chocbar.Salt);
-
+function storeValue(key, value) {
+	//add some code to store the key-value pair in persistant storage 
 }
