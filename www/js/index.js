63d0cce60ed87ccd9fbd197e572639a9c0c7c0fb
rmcoombe <coor2_16@uni.worc.ac.uk>
2017-11-16 15:23:36 +0000

    ftfgb
    vvdfv

--- www/js/index.js
+++ www/js/index.js
@@ -4,11 +4,16 @@ var number = !Math.round(Math.random());
 document.getElementById("output").innerHTML=number;
 
 if (number == true){
-	numbernavigator.notification.beep(1);
+	navigator.notification.beep(1);
 	
 	} 
 if (number == false){
-	numbernavigator.notification.beep(2);
+	navigator.notification.beep(2);
 	} 
 }
 
+/*$("#mybutton").on("click",buttonclicked);
+
+function buttonclicked (){
+	
+}*/	
