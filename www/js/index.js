e202179dcc0fd0d284572ee249225d4fa1fed922
rmcoombe <coor2_16@uni.worc.ac.uk>
2017-11-16 14:35:31 +0000

    kjbnj
    ljjm

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,3 @@
-<!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
-
-$(document).on("pagecreate","#pageone",function(){
-  
-  	$('#tapholdtext').on("taphold",function(){
-    	$(this).hide();
- 	});                       
-
-	$('#taptext').on("tap",function(){
-    	$(this).css('color', 'red');
- 	}); 
-
-	$('#swipetext').on("swipeleft",function(){
-    	$(this).css('color', 'green');
-  	});   
-
-
-});
 
 function random() {
 var number = !Math.round(Math.random());
@@ -24,11 +6,8 @@ if (number == true){
 	numbernavigator.notification.beep(1);
 	
 	} 
-else if (number ==false){
+else if (number == false){
 	numbernavigator.notification.beep(2);
 	} 
 }
 
-function beep(){
-	numbernavigator.notification.beep(1);
-}
